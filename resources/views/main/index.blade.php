909aa65e323c3b9bf1f1f8be426b50baddd3d16d
Zachran Razendra <zachranraze@recodex.id>
2025-04-27 12:08:59 +0700

    feat: add MainController, routes, and views for home page
    This commit introduces a new MainController to handle the home page logic, updates the web routes to use the MainController, and adds the necessary Blade views for the home page layout and content. These changes are part of setting up the initial structure for the home page.

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,94 @@
+<x-layouts.main>
+    <div class="container mx-auto py-20">
+        <div class="bg-text/10 rounded-4xl px-4 sm:px-6 lg:px-8">
+            <div class="h-36 flex justify-around items-center">
+                <div class="text-center">
+                    <p class="text-4xl font-semibold">2000+</p>
+                    <h3 class="text-xl">Anjay</h3>
+                </div>
+                <div class="text-center">
+                    <p class="text-4xl font-semibold">2000+</p>
+                    <h3 class="text-xl">Anjay</h3>
+                </div>
+                <div class="text-center">
+                    <p class="text-4xl font-semibold">2000+</p>
+                    <h3 class="text-xl">Anjay</h3>
+                </div>
+                <div class="text-center">
+                    <p class="text-4xl font-semibold">2000+</p>
+                    <h3 class="text-xl">Anjay</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mx-auto py-20">
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-2 gap-20 mb-8">
+                <h3 class="text-5xl font-semibold">Kami Membantu Bisnis Anda Meraih Potensi Maksimal di Dunia Digital</h3>
+
+                <p class="text-lg font-medium">Recodex ID adalah perusahaan jasa pembuatan website profesional yang didedikasikan untuk membantu bisnis dari berbagai skala meraih potensi maksimal mereka di dunia digital. Didirikan dengan semangat untuk menggabungkan teknologi mutakhir dan desain kreatif, kami hadir sebagai mitra digital terpercaya bagi klien yang menginginkan kehadiran online yang kuat dan efektif.</p>
+            </div>
+            <div class="flex items-start space-x-8">
+                <img class="mt-10 rounded-4xl w-[900px] h-[600px] object-cover" src="{{ asset('images/1.jpg') }}">
+
+                <img class="rounded-4xl w-full h-[720px] object-cover" src="{{ asset('images/2.jpg') }}">
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-text/10 rounded-[64px]">
+        <div class="container mx-auto py-20">
+            <div class="flex space-x-8 px-4 sm:px-6 lg:px-8">
+                <div class="w-2/5">
+                    <h3 class="text-5xl font-semibold mb-4">Layanan Kami</h3>
+                    <p class="text-lg font-medium mb-8">Recodex ID adalah perusahaan jasa pembuatan website profesional yang didedikasikan untuk membantu bisnis.</p>
+
+                    <div class="space-y-8">
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">01</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">02</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">03</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">04</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">05</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                        <div class="bg-background rounded-full p-4 flex items-center justify-around text-xl">
+                            <p class="text-2xl">06</p>
+                            <h2 class="font-semibold">Web Development</h2>
+                            <flux:icon.arrow-right />
+                        </div>
+                    </div>
+                </div>
+                <div class="w-2/5">
+                    <img class="rounded-4xl w-full h-[720px] object-cover" src="{{ asset('images/2.jpg') }}">
+                </div>
+                <div class="w-1/5 space-y-5">
+                    <div class="bg-background rounded-4xl h-[350px]">
+
+                    </div>
+                    <div class="bg-primary rounded-4xl h-[350px]">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.main>
